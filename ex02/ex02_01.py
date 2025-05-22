e7a04eb0b27460d395f847e7e7bffedbfa1cebf7
quochuy1902 <nguyenquochuy1902.k4@gmail.com>
2025-05-22 23:26:12 +0700

    Add files via upload

---
+++ ex02/ex02_01.py
@@ -0,0 +1,5 @@
+1 # Nhập tên và tuổi từ người dùng
+ten = input("Nhập tên của bạn: ")
+tuoi = input("Nhập tuổi của bạn: ")
+4 # In thông điệp chào mừng với thông tin vừa nhập 5 print("Chào mừng,", ten, "! Bạn", tuoi, "tuổi.")
+print("chao mung ,",ten,"! bạn", tuoi , "tuoi.")
